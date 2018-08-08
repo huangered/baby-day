f25958ecb1b1d827531d5c352229fc74bf18e7c4
Peter Huang <peter.huang@farferch.com>
2018-08-08 10:05:24 +0800

    Initial Commit

---
+++ bady-day/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bady-day
+//
+//  Created by peter.huang on 2018/8/8.
+//  Copyright © 2018 peter.huang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ bady-dayTests/bady_dayTests.swift
@@ -0,0 +1,36 @@
+//
+//  bady_dayTests.swift
+//  bady-dayTests
+//
+//  Created by peter.huang on 2018/8/8.
+//  Copyright © 2018 peter.huang. All rights reserved.
+//
+
+import XCTest
+@testable import bady_day
+
+class bady_dayTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
